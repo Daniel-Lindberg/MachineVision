34b0a0d5bf72bcb0e790de2364b928c67e3ee81c
Daniel-Lindberg <Daniel.Lindberg44@gmail.com>
2018-08-12 12:39:02 -0600

    Added C++ Code

--- Exercise6/FasterPedestrian.cpp
+++ Exercise6/FasterPedestrian.cpp
@@ -1,119 +1,126 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html
 
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/video/tracking.hpp"
-#include <vector>
-#include <stdio.h>
-//#include <Windows.h>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
 #include <iostream>
-#include <time.h>
-#include <ctime>
+#include <iomanip>
 
 using namespace cv;
 using namespace std;
 
-
-
-int main(int argc, const char** argv)
+class Detector
 {
-
- 
-         // prepare video input
- VideoCapture cap("input.mov");
- 
-         // prepare video output
- VideoWriter outputVideo;
-
-//outputVideo.open("video4.wmv", CV_FOURCC('W', 'M', 'V', '2'), cap.get(CV_CAP_PROP_FPS), Size(640, 480), true);
-
-        // prepare cascadeClassifier
- CascadeClassifier detectorBody;
- CascadeClassifier detectorUpper;
- // !! Put your cascade or opencv cascede into project folder !!
- string cascadeName1 = "cascadeName.xml";
- string cascadeName2 = "cascadeName.xml";
-
-        // Load cascade into CascadeClassifier
- bool loaded1 = detectorBody.load(cascadeName1);
- bool loaded3 = detectorUpper.load(cascadeName2);
-
-
-
- 
-
-         // Basic video input loop
- for (;;)
- {
-
-  bool Is = cap.grab();
-  if (Is == false) {
-
-   cout << "Video Capture Fail" << endl;
-   break;
-  }
-  else {
-                         // Just for measure time   
-   const clock_t begin_time = clock();
-
-                        // Store results in these 2 vectors
-   vector<Rect> human;
-   vector<Rect> upperBody;
-
-                        // prepare 2 Mat container
-   Mat img;
-   Mat original;
-
-   // capture frame from video file
-   cap.retrieve(img, CV_CAP_OPENNI_BGR_IMAGE);
-   // Resize image if you want with same size as your VideoWriter
-   resize(img, img, Size(640, 480));
-                        // Store original colored image
-   img.copyTo(original);
-                        // color to gray image
-   cvtColor(img, img, CV_BGR2GRAY);
-
-                       // detect people, more remarks in performace section  
-detectorBody.detectMultiScale(img, human, 1.1, 2, 0 | 1, Size(40,70), Size(80, 300));
-detectorUpper.detectMultiScale(img, upperBody, 1.1, 2, 0 | 1, Size(40, 70), Size(80, 300));
-
-   
-
-                        // Draw results from detectorBody into original colored image
-   if (human.size() > 0) {
-    for (int gg = 0; gg < human.size(); gg++) {
-
-    rectangle(original, human[gg].tl(), human[gg].br(), Scalar(0,0,255), 2, 8, 0);
-
+    enum Mode { Default, Daimler } m;
+    HOGDescriptor hog, hog_d;
+public:
+    Detector() : m(Default), hog(), hog_d(Size(48, 96), Size(16, 16), Size(8, 8), Size(8, 8), 9)
+    {
+        hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+        hog_d.setSVMDetector(HOGDescriptor::getDaimlerPeopleDetector());
     }
-   }
-
-   
-                       // Draw results from detectorUpper into original colored image
-   if (upperBody.size() > 0) {
-    for (int gg = 0; gg < upperBody.size(); gg++) {
-
-         rectangle(original, upperBody[gg].tl(), upperBody[gg].br(),  Scalar(255,0,0), 2, 8, 0);
-
+    void toggleMode() { m = (m == Default ? Daimler : Default); }
+    string modeName() const { return (m == Default ? "Default" : "Daimler"); }
+    vector<Rect> detect(InputArray img)
+    {
+        // Run the detector with default parameters. to get a higher hit-rate
+        // (and more false alarms, respectively), decrease the hitThreshold and
+        // groupThreshold (set groupThreshold to 0 to turn off the grouping completely).
+        vector<Rect> found;
+        if (m == Default)
+            hog.detectMultiScale(img, found, 0, Size(8,8), Size(32,32), 1.05, 2, false);
+        else if (m == Daimler)
+            hog_d.detectMultiScale(img, found, 0.5, Size(8,8), Size(32,32), 1.05, 2, true);
+        return found;
     }
-   }
-           // measure time as current - begin_time
-          clock_t diff = clock() - begin_time;
-                                // convert time into string
-          char buffer[126];
-          sprintf(buffer, "%d",diff);
-           // display TIME ms on original image
-           putText(original, buffer, Point(100, 20), 1, 2, Scalar(255, 255, 255), 2, 8, 0);
-           putText(original, "ms" , Point(150, 20), 1, 2, Scalar(255, 255, 255), 2, 8, 0);
-                                // draw results
-    namedWindow("prew", WINDOW_AUTOSIZE);
-    imshow("prew", original);
-                                // make video output
-    outputVideo << original; 
+    void adjustRect(Rect & r) const
+    {
+        // The HOG detector returns slightly larger rectangles than the real objects,
+        // so we slightly shrink the rectangles to get a nicer output.
+        r.x += cvRound(r.width*0.1);
+        r.width = cvRound(r.width*0.8);
+        r.y += cvRound(r.height*0.07);
+        r.height = cvRound(r.height*0.8);
+    }
+};
 
-    int key1 = waitKey(20);
+static const string keys = "{ help h   |   | print help message }"
+                           "{ camera c | 0 | capture video from camera (device index starting from 0) }"
+                           "{ video v  |   | use video as input }";
 
-  }
- }
+int main(int argc, char** argv)
+{
+    CommandLineParser parser(argc, argv, keys);
+    parser.about("This sample demonstrates the use ot the HoG descriptor.");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+    int camera = parser.get<int>("camera");
+    string file = parser.get<string>("video");
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 1;
+    }
+
+    VideoCapture cap;
+    if (file.empty())
+        cap.open(camera);
+    else
+        cap.open(file.c_str());
+    if (!cap.isOpened())
+    {
+        cout << "Can not open video stream: '" << (file.empty() ? "<camera>" : file) << "'" << endl;
+        return 2;
+    }
+
+    cout << "Press 'q' or <ESC> to quit." << endl;
+    cout << "Press <space> to toggle between Default and Daimler detector" << endl;
+    Detector detector;
+    Mat frame;
+    for (;;)
+    {
+        cap >> frame;
+        if (frame.empty())
+        {
+            cout << "Finished reading: empty frame" << endl;
+            break;
+        }
+        int64 t = getTickCount();
+        vector<Rect> found = detector.detect(frame);
+        t = getTickCount() - t;
+
+        // show the window
+        {
+            ostringstream buf;
+            buf << "Mode: " << detector.modeName() << " ||| "
+                << "FPS: " << fixed << setprecision(1) << (getTickFrequency() / (double)t);
+            putText(frame, buf.str(), Point(10, 30), FONT_HERSHEY_PLAIN, 2.0, Scalar(0, 0, 255), 2, LINE_AA);
+        }
+        for (vector<Rect>::iterator i = found.begin(); i != found.end(); ++i)
+        {
+            Rect &r = *i;
+            detector.adjustRect(r);
+            rectangle(frame, r.tl(), r.br(), cv::Scalar(0, 255, 0), 2);
+        }
+        imshow("People detector", frame);
+
+        // interact with user
+        const char key = (char)waitKey(30);
+        if (key == 27 || key == 'q') // ESC
+        {
+            cout << "Exit requested" << endl;
+            break;
+        }
+        else if (key == ' ')
+        {
+            detector.toggleMode();
+        }
+    }
+    return 0;
 }
-
